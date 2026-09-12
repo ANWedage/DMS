1737e6ab16d744e76c8f8da5c58197acf85f1b31
Adeepa Wedage <IT23826304@my.sliit.lk>
2026-09-12 21:21:00 +0530

    Revert "Add self study daily updates and bump version to 1.3.15"
    This reverts commit 31cd3062524992e3349ddfba478ecd6d5cc617a4.

--- Models/TaskModels.cs
+++ Models/TaskModels.cs
@@ -26,12 +26,6 @@ public static class TaskPriorities
     public const string High = "High";
 }
 
-public static class DefaultTaskEntries
-{
-    public const string SelfStudyProjectId = "__SELF_STUDY_PROJECT__";
-    public const string SelfStudyComponentId = "__SELF_STUDY__";
-}
-
 public class TaskProject
 {
     [BsonId]
